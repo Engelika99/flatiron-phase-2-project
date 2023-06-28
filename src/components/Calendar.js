2b93fb3bc05e8ee059b6048ac958e5b472c55f77
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-06-28 15:59:03 -0400

    change Calendar component add react icon change api change format

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,48 +1,132 @@
 /** @format */
 
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { WiRain, WiSnow, WiCloudy, WiThermometer } from "react-icons/wi";
 import "./Calendar.css";
 
 function Calendar() {
+  const [currentDate, setCurrentDate] = useState(new Date());
   const [weatherData, setWeatherData] = useState([]);
 
-  useEffect(() => {
-    fetchWeatherData();
-  }, []);
+  const handlePrevMonth = () => {
+    setCurrentDate((prevDate) => new Date(prevDate.getFullYear(), prevDate.getMonth() - 1, 1));
+  };
 
-  const fetchWeatherData = async () => {
+  const handleNextMonth = () => {
+    setCurrentDate((prevDate) => new Date(prevDate.getFullYear(), prevDate.getMonth() + 1, 1));
+  };
+
+  const getMonthName = (date) => {
+    return date.toLocaleString("default", { month: "long" });
+  };
+
+  const fetchWeatherData = async (latitude, longitude) => {
     try {
-      const response = await fetch("http://localhost:3001/weather");
+      const response = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,precipitation_probability,rain,showers,snowfall,cloudcover,temperature_80m,temperature_120m&timezone=America%2FNew_York&forecast_days=16&models=best_match`
+      );
       const data = await response.json();
-      setWeatherData(data);
+      return data;
     } catch (error) {
       console.log(error);
+      return null;
     }
   };
 
+  const getWeatherIcon = (precipitationProbability) => {
+    if (precipitationProbability >= 70) {
+      return <WiRain />;
+    } else if (precipitationProbability >= 40) {
+      return <WiSnow />;
+    } else {
+      return <WiCloudy />;
+    }
+  };
+
+  useEffect(() => {
+    const renderCalendar = async () => {
+      const year = currentDate.getFullYear();
+      const month = currentDate.getMonth();
+      const firstDayOfMonth = new Date(year, month, 1);
+      const startingDay = firstDayOfMonth.getDay();
+      const numDays = new Date(year, month + 1, 0).getDate();
+
+      const calendarRows = [];
+      let calendarCells = [];
+
+      for (let i = 0; i < startingDay; i++) {
+        calendarCells.push(<td key={`empty-${i}`} className="empty-cell" />);
+      }
+
+      for (let day = 1; day <= numDays; day++) {
+        const weather = await fetchWeatherData(40.7143, -74.006); // New York coordinates
+
+        // Extract the temperature and precipitation probability for the current day
+        const hourlyData = weather.hourly;
+        const startIndex = day - 1;
+        const endIndex = day * 24;
+        const temperatureData = hourlyData.temperature_2m.slice(startIndex, endIndex);
+        const precipitationProbabilityData = hourlyData.precipitation_probability.slice(startIndex, endIndex);
+
+        // Calculate the average temperature and maximum precipitation probability for the day
+        const averageTemperature = temperatureData.reduce((sum, temperature) => sum + temperature, 0) / 24;
+        const maxPrecipitationProbability = Math.max(...precipitationProbabilityData);
+
+        calendarCells.push(
+          <td key={day} className="date-cell">
+            <div className="date">{day}</div>
+            <div className="temperature">
+              <WiThermometer /> {averageTemperature.toFixed(1)}°C
+            </div>
+            <div className="precipitation-probability">
+              {maxPrecipitationProbability}% {getWeatherIcon(maxPrecipitationProbability)}
+            </div>
+          </td>
+        );
+
+        if (calendarCells.length === 7) {
+          calendarRows.push(<tr key={`row-${calendarRows.length}`}>{calendarCells}</tr>);
+          calendarCells = [];
+        }
+      }
+
+      if (calendarCells.length > 0) {
+        while (calendarCells.length < 7) {
+          calendarCells.push(<td key={`empty-${calendarCells.length}`} className="empty-cell" />);
+        }
+        calendarRows.push(<tr key={`row-${calendarRows.length}`}>{calendarCells}</tr>);
+      }
+
+      setWeatherData(calendarRows);
+    };
+
+    renderCalendar();
+  }, [currentDate]);
+
   return (
-    <div>
-      <h2>Calendar</h2>
+    <div className="calendar">
+      <div className="header">
+        <button className="prev-month" onClick={handlePrevMonth}>
+          &lt;
+        </button>
+        <div className="month">{getMonthName(currentDate)}</div>
+        <button className="next-month" onClick={handleNextMonth}>
+          &gt;
+        </button>
+      </div>
       <table>
         <thead>
           <tr>
-            <th>Date</th>
-            <th>Weather</th>
-            <th>Details</th>
+            <th>Sun</th>
+            <th>Mon</th>
+            <th>Tue</th>
+            <th>Wed</th>
+            <th>Thu</th>
+            <th>Fri</th>
+            <th>Sat</th>
           </tr>
         </thead>
-        <tbody>
-          {weatherData.map((day) => (
-            <tr key={day.date}>
-              <td>{day.date}</td>
-              <td>{day.weather}</td>
-              <td>
-                <Link to={`/weather/${day.date}`}>View Details</Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+        <tbody>{weatherData}</tbody>
       </table>
     </div>
   );
