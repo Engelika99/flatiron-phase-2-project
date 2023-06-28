c9992a07450481768c43289bca02771cae1ecb10
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-06-28 11:18:55 -0400

    Find more calendar typos

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ function Home() {
       <h2>Welcome To Fashionable In Any Weather Baby App!</h2>
       <p>This app provides weather information for your baby's outdoor activities.</p>
       <p>
-        <Link to="/calendar">View Weather Calander</Link>
+        <Link to="/calendar">View Weather Calendar</Link>
       </p>
     </div>
   );
