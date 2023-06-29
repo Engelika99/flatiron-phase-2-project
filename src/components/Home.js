a65d0f7511597d07d21f7c60755e44711a985d89
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-06-28 22:04:11 -0400

    create home component css file

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,10 +2,11 @@
 
 import React from "react";
 import { Link } from "react-router-dom";
+import "./home.css";
 
 function Home() {
   return (
-    <div>
+    <div className="home">
       <h2>Welcome To Fashionable In Any Weather Baby App!</h2>
       <p>This app provides weather information for your baby's outdoor activities.</p>
       <p>
